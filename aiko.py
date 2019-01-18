a9b3e70f0d7e6ecaa7324420dad39721ea682201
Denis Titov <denecity@yandex.com>
2019-01-18 15:37:28 +0000

    functional version now gives all data

--- aiko.py
+++ aiko.py
@@ -1,20 +1,124 @@
 #aiko package
 #Denis Titov
 #16.01.2019
-#
 
 import h5py
 import numpy as np
 import sys
 import os
 import pandas as pd
+import re
 
-"""
-TODO:
+#class for reading/writing/handlin of snapshot directories, managed by aiko
+class snapshot_read():
+    #sets path of snapshot dir (corrects for missing slash at end of path) and other vars
+    def __init__(self, snapPath, blockNames, particleTypes):
+        if snapPath[-1] is not "/":
+            snapPath = snapPath + "/"
+        self.snapPath = snapPath
+        self.blockNames = blockNames
+        self.particleTypes = particleTypes
+        self.particleTypeIndexes = self.get_particleType_index()
+        self.numSnapdirs = self.get_numSnapdirs()
+        self.currentSnapshot = self.get_snapshot()
 
-read FILE FOLDER ETC.
+    #searches for a file with correct name, opens and gets number of snaps from header
+    def get_numSnapdirs(self):
+        for i in os.listdir(self.snapPath):
+            if re.search("\.\d\.", i):
+                fileName = i
+                break
+        filePath = self.snapPath + fileName
+        file = h5py.File(filePath, 'r')
+        attrs = file['/Header'].attrs
+        numFilesPerSnapshot = attrs["NumFilesPerSnapshot"]
+        return numFilesPerSnapshot
 
-write TO DATABASE, MULTIPLE FILES ETC.
+    #converts words of particleTypes to particleTypeIndexes
+    def get_particleType_index(self):
+        indexList = []
+        index = {"Gas":0, "DM":1, "Darkmatter":1, "Stars":4}
+        for particleType in self.particleTypes:
+            converted = index[particleType]
+            indexList.append(converted)
+        return indexList
 
-convert TO PANDAS, TO NUMPY (SELECTION, SLICE) ETC.
-"""
+    #returns current snapshot
+    def get_snapshot(self):
+        for i in os.listdir(self.snapPath):
+            if re.search("\_\d{3}\.", i):
+                snapshot = re.findall("\_\d{3}\.", i)[0][1:4]
+                return int(snapshot)
+
+    #returns list of all filenames in dir
+    def get_snapdirList(self):
+        return os.listdir(self.snapPath)
+
+    #returns
+    def read_arepo_snap(self):
+        dfSnapList = []
+        for filename in self.get_snapdirList():
+            file = h5py.File(self.snapPath +  filename, "r")
+            dfFileList = []
+            typeList = list(file)
+            print("reading " + filename)
+            for particleTypeIndex in self.particleTypeIndexes:
+                typeStructure = "PartType{}".format(particleTypeIndex)
+                if typeStructure not in typeList:
+                    break
+                dfStructure = {}
+                for blockName in self.blockNames:
+                    h5Path = "/PartType{}/{}".format(particleTypeIndex, blockName)
+                    if blockName == "Coordinates":
+                        coords = np.float32(file[h5Path].value)
+                        dfStructure["posX"] = coords[:,0]
+                        dfStructure["posY"] = coords[:,1]
+                        dfStructure["posZ"] = coords[:,2]
+                    if blockName == "Velocities":
+                        vels = np.float32(file[h5Path].value)
+                        dfStructure["velX"] = vels[:,0]
+                        dfStructure["velY"] = vels[:,1]
+                        dfStructure["velZ"] = vels[:,2]
+                    if blockName == "Density" and particleTypeIndex == 0:
+                        dens = np.float32(file[h5Path].value)
+                        dfStructure["Dens"] = dens
+                ID = np.uint32(file["/PartType{}/{}".format(particleTypeIndex, "ParticleIDs")].value)
+                dfStructure["ID"] = ID
+                dfStructure["Snap"] = ID*0+self.currentSnapshot
+                dfStructure["Type"] = ID*0+particleTypeIndex
+                dfCurrent = pd.DataFrame(dfStructure)
+                dfFileList.append(dfCurrent)
+            dfFile = pd.concat(dfFileList, sort=True)
+            dfSnapList.append(dfFile)
+        dfSnap = pd.concat(dfSnapList, ignore_index=True)
+        return dfSnap
+
+class arepo_reader():
+    #defines vars
+    def __init__(self, path, snapshotRange, particleTypes, blockNames):
+        if path[-1] is not "/":
+            path = path + "/"
+        self.path = path
+        self.snapshots = np.arange(snapshotRange[0], snapshotRange[1]+1, 1)
+        self.particleTypes = particleTypes
+        self.blockNames = blockNames
+
+    #gets list of dirnames for each selected snapshot
+    def get_path(self, snapshot):
+        Snapdir = "snapdir_" + "%03d" % snapshot + "/"
+        path = self.path + Snapdir
+        return path
+
+    # returns full list in df
+    def read_arepo(self):
+        dataList = {}
+        for snapshot in self.snapshots:
+            currentPath = self.get_path(snapshot)
+            currentInstance = snapshot_read(currentPath, particleTypes=self.particleTypes, blockNames=self.blockNames)
+            currentData = currentInstance.read_arepo_snap()
+            dataList[snapshot] = currentData
+        print("processing...")
+        dfAll = pd.concat(dataList, ignore_index=True)
+        dfAll.set_index("ID", inplace=True)
+        print("done!")
+        return dfAll
